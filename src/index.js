4c4e98c86626410afc64a6a281999d5a282ca9ad
Jin <jinruncai@gmail.com>
2019-12-10 18:31:48 -0800

    Install packages. Bootstrap file structure.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const root = document.querySelector("#root");
+const app = (
+    <App></App>
+)
+
+ReactDOM.render(app, root);
